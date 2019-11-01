4bed5c96abd6904f267d3ffaf32905a7d2e02173
cmkishores <cmkishores@gmail.com>
2019-11-01 11:35:18 +0530

    Initial structure of App.js

--- src/App.js
+++ src/App.js
@@ -2,3 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+
+  }
+  
+  
+  render() {
+    return (
+      <div>
+        <header> Kochi Metro Fare Calculator</header>
+
+      </div>
+    )
+  }
+  }
+
+  export default App
+
