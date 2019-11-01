61bbaf0034d161c199c033b4b75e59a4f8584b42
cmkishores <cmkishores@gmail.com>
2019-11-01 15:16:16 +0530

    Change app.js to display 2 drop down fields

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import stationDetails from './stationDetails'
-import Stations from './Stations'
+import { timingSafeEqual } from 'crypto';
+
+
+
+
 
 class App extends React.Component {
   constructor() {
@@ -14,16 +18,39 @@ class App extends React.Component {
   }
   
   componentDidMount() {
-    const stationName = stationDetails.map( stations => {return stationDetails.stop_name} )
+    const stationName = stationDetails.map( stations => {return stations} )
+    console.log(stationName)
     this.setState(
       { stations: stationName }
+       
     )
+    console.log(this.state.stations.stationName)
+  
   }
   
   render() {
-    
+    let stationsList = this.state.stations
+    console.log(stationsList)
+    let options = stationsList.map( (list) => <option key={list.stop_name}> {list.stop_name} </option>  ) 
+    console.log(options)
     return (
-     <Stations stationList={this.state}/>   )
+      <div>
+
+            <h1>Kochi metro Fare calculator</h1>
+            <form>
+            <label>Source station:</label>
+            <select>
+            {options}
+
+            </select>
+            <label>Destination station:</label>
+
+            <select>
+            {options}
+
+            </select>
+            </form>
+        </div>  )
   }
   }
 
