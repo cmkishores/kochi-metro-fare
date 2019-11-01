5df208d2f4b0800ee8f513161590c90d59f83164
cmkishores <cmkishores@gmail.com>
2019-11-01 13:54:41 +0530

    Pass stations list to child component Stations to populate and render options for dropdown

--- src/App.js
+++ src/App.js
@@ -2,35 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import stationDetails from './stationDetails'
+import Stations from './Stations'
 
 class App extends React.Component {
   constructor() {
     super()
-    this.state = {}
-
+    this.state = {
+      stations:[],
+    }
+    this.componentDidMount = this.componentDidMount.bind(this)
   }
   
-
+  componentDidMount() {
+    const stationName = stationDetails.map( stations => {return stationDetails.stop_name} )
+    this.setState(
+      { stations: stationName }
+    )
+  }
   
   render() {
     
     return (
-      <div>
-        <h1> Kochi Metro Fare Calculator</h1>
-        <form>
-        <select 
-                    value={this.state.favColor}
-                    onChange={this.handleChange}
-                    name="favColor"
-                >
-                    <option value="blue">Blue</option>
-               
-                </select>
-
-        </form>
-
-      </div>
-    )
+     <Stations stationList={this.state}/>   )
   }
   }
 
