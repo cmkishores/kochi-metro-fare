b8a67cc1b32af0ea816940c11ccef0e8df9b0268
cmkishores <cmkishores@gmail.com>
2019-11-01 17:04:39 +0530

    Updates

---
+++ precache-manifest.af2aab0dd0244bed93c97186850a4459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf28a69edb2332d10a9d4bdbac2d7e97",
+    "url": "/kochi-metro-fare/index.html"
+  },
+  {
+    "revision": "79de1be6248060fcdb7a",
+    "url": "/kochi-metro-fare/static/css/main.f8965b0e.chunk.css"
+  },
+  {
+    "revision": "3cfeae79781abdd44d86",
+    "url": "/kochi-metro-fare/static/js/2.ab9098c1.chunk.js"
+  },
+  {
+    "revision": "79de1be6248060fcdb7a",
+    "url": "/kochi-metro-fare/static/js/main.e3558ff5.chunk.js"
+  },
+  {
+    "revision": "b551b87c7798713ad774",
+    "url": "/kochi-metro-fare/static/js/runtime-main.73325d96.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/kochi-metro-fare/static/media/logo.25bf045c.svg"
+  }
+]);
